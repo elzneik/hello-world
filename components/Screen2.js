e9b4be0a6d0e76de1d00bf40f5bb0807b43ca6c0
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-24 19:50:30 +0200

    update screen2

--- components/Screen2.js
+++ components/Screen2.js
@@ -5,7 +5,7 @@ import { View, Text, Button} from 'react-native';
 export default class Screen2 extends Component {
   render() {
    // let name = this.props.route.params.name;
-   //  this.props.navigation.setOptions({title:name});
+   // this.props.navigation.setOptions({title:name});
 
     return (
       <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
