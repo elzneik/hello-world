d350f1b4b910c198f7c6c18de7bcf4d2369e0d0f
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-27 19:00:09 +0200

    modify app.js and chat.js

--- App.js
+++ App.js
@@ -1,3 +1,45 @@
+// check code from CF - to get rid of the issue
+import React, { Component } from "react";
+// import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import Chat from "./components/Chat";
+import Start from "./components/Start";
+// import react native gesture handler
+import "react-native-gesture-handler";
+
+// import react Navigation
+import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+  export default class App extends React.Component {
+  //constructor(props) {
+    //super(props);
+    //this.state = { text: "" };
+  //}
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Start">
+          <Tab.Screen name="Start" component={Start} />
+          <Tab.Screen name="Chat" component={Chat} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+// My own code
 /* My code - has issues
 import React, { Component } from 'react';
 //import { StyleSheet, View, TextInput, Text, Button, Alert, ScrollView  } from 'react-native';
@@ -38,42 +80,7 @@ export default class HelloWorld extends Component {
 }
 */
 
-// check code from CF - to get rid of the issue
-import React, { Component } from "react";
-// import { StyleSheet, View, Text, TextInput, Button } from "react-native";
-import Chat from "./components/Chat";
-import Start from "./components/Start";
-// import react native gesture handler
-import "react-native-gesture-handler";
 
-// import react Navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-const Stack = createStackNavigator();
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Start">
-          <Stack.Screen name="Start" component={Start} />
-          <Stack.Screen name="Chat" component={Chat} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-// My own code
 
 
   /*
--- components/Chat.js
+++ components/Chat.js
@@ -1,57 +1,20 @@
-import React from "react";
-import { GiftedChat } from "react-native-gifted-chat";
+import React from 'react';
+import { View, Text, Button} from 'react-native';
 
-export default class Chat extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-       messages: [
-        {
-          _id: 1,
-          text: "Hello developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
-      ],
-    };
-  }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     messages: [
-  //       {
-  //         _id: 1,
-  //         text: "Hello developer",
-  //         createdAt: new Date(),
-  //         user: {
-  //           _id: 2,
-  //           name: "React Native",
-  //           avatar: "https://placeimg.com/140/140/any",
-  //         },
-  //       },
-  //     ],
-  //   });
-  // }
-
-  onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
-  }
-
+export default class Chat extends Component {
   render() {
+   let name = this.props.route.params.name;
+   this.props.navigation.setOptions({title:name});
+
     return (
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={(messages) => this.onSend(messages)}
-        user={{
-          _id: 1,
-        }}
-      />
+      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Hello There!</Text>
+        < Button
+            title="Go to Start"
+            onPress={() => this.props.navigation.navigate("Start")}
+        />
+      </View>
     );
   }
 }
