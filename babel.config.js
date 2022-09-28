a62d40cc81dc970ad51d15d459a8d7feddfac342
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-28 13:01:00 +0200

    update babel

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    //presets: ['babel-preset-expo'],
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: ['babel-preset-expo'],
+    // presets: ['module:metro-react-native-babel-preset'],
+    // plugins: ['react-native-reanimated/plugin'],
   };
 };
