85863a80b717cdfcf4076f16f896fb7836d1503d
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-28 12:08:03 +0200

    update app.js and start.js

--- App.js
+++ App.js
@@ -1,232 +1,26 @@
-// check code from CF - to get rid of the issue
 import React, { Component } from "react";
-// import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import "react-native-gesture-handler";
 import Chat from "./components/Chat";
 import Start from "./components/Start";
-// import react native gesture handler
-import "react-native-gesture-handler";
 
 // import react Navigation
 import { NavigationContainer } from "@react-navigation/native";
-// import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from "@react-navigation/stack";
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-  export default class App extends React.Component {
-  //constructor(props) {
-    //super(props);
-    //this.state = { text: "" };
-  //}
-
-  render() {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName="Start">
-          <Tab.Screen name="Start" component={Start} />
-          <Tab.Screen name="Chat" component={Chat} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-// My own code
-/* My code - has issues
-import React, { Component } from 'react';
-//import { StyleSheet, View, TextInput, Text, Button, Alert, ScrollView  } from 'react-native';
-import Screen1 from './components/Screen1';
-import Screen2 from './components/Screen2';
-import 'react-native-gesture-handler';
-// import react Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
+//Create the navigator
 const Stack = createStackNavigator();
-
-export default class HelloWorld extends Component {
-
-  //constructor (props) {
-  //  super(props);
-  //   this.state = { text: " "};
-  // }
-
+// const Tab = createBottomTabNavigator();
+  export default class App extends React.Component {
+ 
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Screen1"
-        >
-          <Stack.Screen
-            name="Screen1"
-            component={Screen1}
-          />
-          <Stack.Screen
-            name="Screen2"
-            component={Screen2}
-          />
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
         </Stack.Navigator>
       </NavigationContainer>
     );
   }
 }
-*/
-
-
-
-
-  /*
-
-
-  alertMyText (input =[]) {
-    Alert.alert(input.text);
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.box1}></View>
-        <ScrollView>
-          <Text style={{fontSize:110}}>
-            This text is so big! And so long! You have to scroll!
-          </Text>
-        </ScrollView>
-        <View style={styles.box2}></View>
-        <View style={{flex:10, justifyContent: "center"}}>
-          <TextInput
-            style={{height: 50, borderColor: "gray", borderWidth: 1}}
-            onChangeText={text => this.setState({text})}
-            value={this.state.text}
-            placeholder="Type here..."
-          />
-          <Text>You wrote: {this.state.text}</Text>
-        <Button
-          onProgress={() => {
-            this.alertMyText({text: this.state.text});
-          }}
-          title="Press Me"
-          />
-        <View style={styles.box3}></View>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: "yellow"
-  },
-  box1: {
-    //flex:10,
-    width:50,
-    height: 50,
-    backgroundColor: 'blue'
-  },
-  box2: {
-    flex:20,
-    backgroundColor: 'red'
-  },
-  box3: {
-    flex:10,
-    backgroundColor: 'green'
-  }
-});
-*/
-
-
-
-/*
-My code does not work?! Why? 
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-
-export default class HelloWorld extends Component {
-  render() {
-    return (
-      <View style={styles.parentView}>
-          <View style={styles.containerOne}></View>
-          <View style={styles.containerTwo}></View>
-          <View style={styles.containerThree}></View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  parentView: {
-    flex:1,
-    flexDirection: "row",
-  },
-  containerOne: {
-    flex:100,
-    backgroundColor: "#fa6d98",  
-  },
-  containerTwo: {
-    flex:50,
-    backgroundColor: "#fa6d98",   
-  },
-  containerThree: {
-    flex:40,
-    backgroundColor: "#fa6d98", 
-  },
-});
-*/
-    //backgroundColor: "#ffff99",
-
-
-/*
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View>
-      <Text style={styles.blue}>Hello World!</Text>
-      <Text style={styles.bigRed}>How are you?</Text>
-      <Text style={styles.bigRedBold}>I am feeling good!</Text>
-      <Text style={[styles.blue, styles.bigRed]}>I'm feeling blue!</Text>
-      <View style={styles.box}></View>
-      <StatusBar style="auto" />
-    </View>
-     
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-   // backgroundColor: '#ffff99',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  blue: {
-    color: '#0066ff',
-    fontWeight: "600",
-  },
-  bigRed: {
-    color: 'red',
-    fontSize: 30,
-  },
-  bigRedBold: {
-    color: 'red',
-    fontSize: 30,
-    fontWeight: '600',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    backgroundColor: 'blue',
-  }
-});
-*/
--- components/Start.js
+++ components/Start.js
@@ -1,28 +1,81 @@
 import React from "react";
-import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from "react-native";
 
-export default class Start extends React.Component {
+export default class Start extends Component {
   constructor(props) {
     super(props);
     this.state = { name: "" };
   }
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello Screen1!</Text>
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          onChangeText={(name) => this.setState({ name })}
-          value={this.state.name}
-          placeholder="Type here ..."
-        />
-        <Button
-          title="Go to Chat"
-          onPress={() =>
-            this.props.navigation.navigate("Chat", { name: this.state.name })
-          }
-        />
+      <View style={styles.container}>
+        <ImageBackground 
+            source={require('../picture/BackgroundImage.png')} 
+            style={styles.image}>
+          <Text style={styles.appTitle}>Hello World Chat</Text>
+          <View style={styles.nameBox}> 
+            <TextInput
+              style={styles.nameInput}
+              onChangeText={(name) => this.setState({ name })}
+              value={this.state.name}
+              placeholder="Your Name"
+            />
+
+            <Button
+              title="Start Chatting"
+              onPress={() =>
+                this.props.navigation.navigate("Chat", { name: this.state.name })
+              }
+            />
+          </View>
+        </ImageBackground>
       </View>
     );
   }
 }
+
+// Color options HEX codes: #090C08; #474056; #8A95A5; #B9C6AE
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center",
+    backgroundColor: "#090C08",
+    fontSize: 16,
+    fontWeight: 300,
+    color: "#757083",
+    //opacity: 100%,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center", 
+    alignItems: "center",
+    width: '100%', 
+    height: '100%',
+  },
+  appTitle: {
+    fontSize: 45,
+    fontWeight: 600,
+    color: "#FFFFFF",
+  },
+  nameBox: { 
+    height: 40,
+    width: 80, 
+    borderColor: "#fff", 
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  nameInput: {
+    fontSize: 16,
+    fontWeight: 300,
+    color: "#757083",
+    // opacity: 50%,
+  },
+  chatButton: {
+    fontSize: 16,
+    fontWeight: 600,
+    color: "#FFFFFF",
+  },
+});
