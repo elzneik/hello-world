bba4f12cf4724a2ab6927e625ad082b8ac881c66
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-29 12:15:38 +0200

    update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet} from 'react-native';
+// import { View, Text, Button, StyleSheet} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+
 
 
 export default class Chat extends Component {
-  render() {
-   let name = this.props.route.params.name;
-   this.props.navigation.setOptions({title:name});
 
-    return (
-      <View style={styles.container}>
-        <Text>Welcome to the Chat</Text>
-        < Button
-            style={styles.chatButton}
-            title="Go to Start"
-            onPress={() => this.props.navigation.navigate("Start")}
-        />
-      </View>
-    );
+    constructor () {
+      super ();
+      this.state = {
+        messages: [],
+      }
+    }
+
+    componentDidMount() {
+      this.setState({
+        messages: [
+          {
+            _id: 1,
+            text: 'Hello developer',
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: 'React Native',
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+          },
+        ],
+      })
+    }
+
+    onSend(messages = []) {
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }))
+    }
+
+    render () {
+      return (
+        <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: { 
-    flex: 1,
-    justifyContent: 'center', 
-    alignItems: 'center'
-  },
-   chatButton: {
-    fontSize: 16,
-    fontWeight: "600",
-    color: "#FFFFFF",
-  },
-});
